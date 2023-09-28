06248cb31e598c898d4f93c64ff9ec60ab60e4ed
FabioPortella <fabioportella2009@gmail.com>
2023-09-28 20:09:21 -0300

    TextField escondendo a senha

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
+  bool isObscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -95,27 +96,38 @@ class _LoginPageState extends State<LoginPage> {
                     height: 30,
                     alignment: Alignment.center,
                     child: TextField(
+                      obscureText: isObscureText,
                       onChanged: (value) {
                         senha = value;
                       },
                       style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.only(top: 0),
-                          enabledBorder: UnderlineInputBorder(
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(top: 0),
+                          enabledBorder: const UnderlineInputBorder(
                               borderSide: BorderSide(
                                   color: Color.fromARGB(255, 137, 76, 150))),
-                          focusedBorder: UnderlineInputBorder(
+                          focusedBorder: const UnderlineInputBorder(
                               borderSide: BorderSide(
                                   color: Color.fromARGB(255, 137, 76, 150))),
                           hintText: "Senha",
-                          hintStyle: TextStyle(color: Colors.white),
-                          prefixIcon: Icon(
+                          hintStyle: const TextStyle(color: Colors.white),
+                          prefixIcon: const Icon(
                             Icons.lock,
                             color: Color.fromARGB(255, 137, 76, 150),
                           ),
-                          suffixIcon: Icon(
-                            Icons.visibility,
-                            color: Color.fromARGB(255, 137, 76, 150),
+                          suffixIcon: GestureDetector(
+                            // pode usar também InkWell, mas GestureDetector é mais completo
+                            onTap: () {
+                              setState(() {
+                                isObscureText = !isObscureText;
+                              });
+                            },
+                            child: Icon(
+                              isObscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: const Color.fromARGB(255, 137, 76, 150),
+                            ),
                           )),
                     ),
                   ),
