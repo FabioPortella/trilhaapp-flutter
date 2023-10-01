22380b5f86cdd83460e325c6d35ca53fb8d1494c
FabioPortella <fabioportella2009@gmail.com>
2023-09-30 22:21:39 -0300

    Conhecendo o Drawer

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // retira a faixa DEBUG do app
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blueGrey,
         textTheme: GoogleFonts.robotoTextTheme(),
       ),
       home: const LoginPage(),
