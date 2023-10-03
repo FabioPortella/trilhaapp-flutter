9d9cfbd707d5db536718d91a5e8cff49750b905c
FabioPortella <fabioportella2009@gmail.com>
2023-10-03 09:14:15 -0300

    Conhecendo DatePicker

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/shared/widgets/text_label.dart';
 
 class DadosCadastraisPage extends StatefulWidget {
-  DadosCadastraisPage({Key? key}) : super(key: key);
+  const DadosCadastraisPage({Key? key}) : super(key: key);
 
   @override
   State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
 }
 
 class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
-  TextEditingController nomeController = TextEditingController(text: "");
+  var nomeController = TextEditingController(text: "");
+  var dataNascimentoController = TextEditingController(text: "");
+  DateTime? dataNascimento;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Meus dados")),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(
-            "Nome",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-          ),
+          const TextLabel(texto: "Nome"),
           TextField(
             controller: nomeController,
           ),
+          const SizedBox(
+            height: 15,
+          ),
+          const TextLabel(texto: "Data de nascimento"),
+          TextField(
+            controller: dataNascimentoController,
+            readOnly: true,
+            onTap: () async {
+              var data = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime(2000, 1, 1),
+                  firstDate: DateTime(1900, 5, 20),
+                  lastDate: DateTime(2024, 1, 1));
+              if (data != null) {
+                dataNascimentoController.text = data.toString();
+                dataNascimento = data;
+              }
+            },
+          ),
           TextButton(
             onPressed: () {
-              print(nomeController.text);
+              debugPrint(nomeController.text);
+              debugPrint(dataNascimento.toString());
             },
-            child: Text("Salvar"),
+            child: const Text("Salvar"),
           )
         ]),
       ),
