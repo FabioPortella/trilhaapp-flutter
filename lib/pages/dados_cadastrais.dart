5b0157b73ff5c094b53d8a3cca9a4a77aa3bc2f7
FabioPortella <fabioportella2009@gmail.com>
2023-10-03 08:34:29 -0300

    Conhecendo TextField e TextButton

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
-class DadosCadastraisPage extends StatelessWidget {
-  final String texto;
-  final List<String> dados;
-  const DadosCadastraisPage(
-      {Key? key, required this.texto, required this.dados})
-      : super(key: key);
+class DadosCadastraisPage extends StatefulWidget {
+  DadosCadastraisPage({Key? key}) : super(key: key);
+
+  @override
+  State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
+}
+
+class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
+  TextEditingController nomeController = TextEditingController(text: "");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(texto),
+      appBar: AppBar(title: const Text("Meus dados")),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            "Nome",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          ),
+          TextField(
+            controller: nomeController,
+          ),
+          TextButton(
+            onPressed: () {
+              print(nomeController.text);
+            },
+            child: Text("Salvar"),
+          )
+        ]),
       ),
-      body: Center(child: Text(dados.length.toString())),
     );
   }
 }
