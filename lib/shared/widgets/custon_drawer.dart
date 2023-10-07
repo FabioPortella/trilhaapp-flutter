2ffbd29c9981111b4e57cc1b8c9e564f9733917c
FabioPortella <fabioportella2009@gmail.com>
2023-10-07 16:18:36 -0300

    showModalBottomSheet

--- lib/shared/widgets/custon_drawer.dart
+++ lib/shared/widgets/custon_drawer.dart
@@ -10,15 +10,44 @@ class CustonDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(color: Colors.purple),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Image.network(
-                    "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
-              ),
-              accountName: const Text("Fábio Portella"),
-              accountEmail: const Text("email@email.com")),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Câmera"),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Galeria"),
+                          leading: const Icon(Icons.photo_album),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(color: Colors.purple),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
+                ),
+                accountName: const Text("Fábio Portella"),
+                accountEmail: const Text("email@email.com")),
+          ),
           InkWell(
             child: Container(
                 padding:
@@ -59,7 +88,35 @@ class CustonDrawer extends StatelessWidget {
                     Text("Termos de uso e privacidade"),
                   ],
                 )),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 12),
+                      child: const Column(
+                        children: [
+                          Text(
+                            "Termos de Uso e Política de Privacidade.",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            " Este documento estabelece os termos e condições de uso do software XYZ, desenvolvido pela empresa ABC, bem como a política de privacidade que rege o tratamento dos dados pessoais dos usuários do software. Ao instalar, acessar ou utilizar o software XYZ, você concorda em cumprir e respeitar estes termos e condições de uso e política de privacidade. Caso você não concorde com algum dos termos ou condições aqui estabelecidos, você não deve instalar, acessar ou utilizar o software XYZ.",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           const Divider(),
           const SizedBox(
