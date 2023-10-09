091af978d9599d4849ea60bd5142d874eee98747
FabioPortella <fabioportella2009@gmail.com>
2023-10-09 10:34:37 -0300

    Card

---
+++ lib/pages/card_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatefulWidget {
+  const CardPage({super.key});
+
+  @override
+  State<CardPage> createState() => _CardPageState();
+}
+
+class _CardPageState extends State<CardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          width: double.infinity,
+          child: Card(
+            elevation: 8,
+            shadowColor: Colors.grey,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.network(
+                        "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                        height: 20,
+                      ),
+                      const Text(
+                        "Meu Card",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                      "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.normal)),
+                  Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Ler mais",
+                            style:
+                                TextStyle(decoration: TextDecoration.underline),
+                          ))),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
