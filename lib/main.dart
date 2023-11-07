654ade9aa1c845184ec613b229aa877f82079f05
FabioPortella <fabioportella2009@gmail.com>
2023-11-07 09:36:20 -0300

    Adicionando SQLite na lista de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,33 @@ import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:trilhaapp/model/dados_cadastrais_model.dart';
 import 'package:trilhaapp/model/tarefa_hive_model.dart';
 import 'my_app.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+
+Map<int, String> scripts = {
+  1: ''' CREATE TABLE tarefas (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         descricao TEXT,
+         concluido INTEGER
+         );'''
+};
+
+Future iniciarBancoDeDados() async {
+  var db = await openDatabase(
+      path.join(await getDatabasesPath(), 'database.db'),
+      version: scripts.length, onCreate: (Database db, int version) async {
+    for (var i = 1; i <= scripts.length; i++) {
+      await db.execute(scripts[i]!);
+      debugPrint(scripts[i]!);
+    }
+  }, onUpgrade: (Database db, int oldVersion, int newVersion) async {
+    for (var i = oldVersion + 1; i <= scripts.length; i++) {
+      await db.execute(scripts[i]!);
+      debugPrint(scripts[i]!);
+    }
+  });
+  return db;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,5 +39,6 @@ void main() async {
   Hive.init(documentsDirectory.path);
   Hive.registerAdapter(DadosCadastraisModelAdapter());
   Hive.registerAdapter(TarefaHiveModelAdapter());
+  await iniciarBancoDeDados();
   runApp(const MyApp());
 }
